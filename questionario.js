05458d9f3cba95ba2397c6c9396bfd30bbfa4fba
Dario Russo <dariorusso987@gmail.com>
2023-10-10 09:55:34 +0200

    Create questionario.js

---
+++ questionario.js
@@ -0,0 +1,29 @@
+// Pratica - Mod. 2 - Week 1 - D1 //////////////////////////////////////////////////////////////////////////////////
+//
+//
+// 1. Cos'è un algoritmo? prova a spiegarlo con parole tue, come se lo dovessi spiagare a qualcuno che
+// non ne capisce niente di informatica.
+//
+// Risposta: un algoritmo è una sequenza ordinata di istruzioni, di azioni da compiere, affinché si possa portare 
+// a termine un determinato compito. Un algoritmo può ricevere in ingresso dei dati , gli input, e può produrre
+// dati in uscita, output.
+//
+//
+//
+// 2. Cos'è una variabile? prova a spiegarlo con parole tue, come se lo dovessi spiagare a qualcuno che
+// non ne capisce niente di informatica.
+//
+// Risposta: Una variabile la possiamo immaginare come un contenitore il cui scopo sarà proprio quello
+// di contenere qualcosa che sarà utilizzato al momento opportuno. Può contenere "cose" di tipo diverso e
+// si può modificarne il contenuto nel tempo.
+//
+//
+//
+// 3. Undefined e null sono la stessa cosa?
+// 
+// Risposta: No, non sono la stessa cosa anche se apparentemente sembrano avere lo stesso comportamento.
+// Undefined è il valore che assume una variabile di default quando non le si assegna nulla, null è un 
+// valore che viene assegnato alla variabile di proposito.
+//
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
